7514bf90fbac2cf6dc7780e4f6997f055bae79c7
zaraky97 <yuya.sasagawa@item-shop.jp>
2022-04-02 19:45:43 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { App } from "@slack/bolt";
+import { google, calendar_v3 } from "googleapis";
 import "dotenv/config";
 
 const app = new App({
@@ -6,12 +7,16 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-app.message("月曜定例", async ({ message, say }) => {
+const calendar = google.calendar({
+  version: "v3",
+  auth: process.env.GOOGLE_API_KEY,
+});
+
+app.message("木曜定例", async ({ message, say }) => {
   await say(`Hey there`);
 });
 
 (async () => {
   await app.start(process.env.PORT || 3000);
-
   console.log("⚡️ Bolt app is running!");
 })();
